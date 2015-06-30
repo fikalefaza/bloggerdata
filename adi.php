f8b50e85679b317c65dbe61d8b709c377eba0168
shofiyudin <shofiyud@gmail.com>
2015-06-30 05:28:06 +0000

    isik

--- adi.php
+++ adi.php
@@ -1,3 +1,18 @@
 <?php
-echo('ksss');
+
+$a=$_POST['aa'];
+$b=$_POST['bb'];
+$c=$_POST['cc'];
+$d=$_POST['dd'];
+$e=md5 ($_POST['ee']);
+$f=$_POST['ff'];
+
+if (!$a && !$b && !$c && !$d && !$e && !$f)
+{
+echo"Data Belum Ditambah";
+}
+else
+{
+	echo "testing tambah";
+}
 ?>
